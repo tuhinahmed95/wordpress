3d1ed833d305a64b0547e330dfeb2aa0b39b74ca
Md Tuhin Mia <mtuhinahmed95@gmail.com>
2024-06-03 22:56:18 +0600

    update

--- wordpresswebsite/functions.php
+++ wordpresswebsite/functions.php
@@ -9,7 +9,12 @@
 
  // theme css and jQuery file calling
  function tuhin_css_js_file_calling(){  
-    wp_enqueue_style('tuhin-style', get_stylesheet_uri())
+    wp_enqueue_style('tuhin-style', get_stylesheet_uri());
+    wp_register_style('bootstrap',get_template_directory_uri()./css/bootstrap.css,array(), )
+   }
+   ad_action('wp_enqueue_scripts', 'tuhin_css_js_file_callin')
+  
+  
  }
  ad_action('wp_enqueue_scripts', 'tuhin_css_js_file_callin')
 
